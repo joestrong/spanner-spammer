f7239bf718b26f1788053ea1b21ceee73457cacb
Joe Strong <joestrong@gmail.com>
2019-07-13 15:22:25 +0100

    Configures target objects

--- src/index.ts
+++ src/index.ts
@@ -28,9 +28,20 @@ const createScene = () => {
 
   const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
 
-  const sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene, false, BABYLON.Mesh.FRONTSIDE);
-  sphere.position.y = 1;
-  sphere.physicsImpostor = new BABYLON.PhysicsImpostor(sphere, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
+  const box = BABYLON.Mesh.CreateBox('box1', 2, scene, false, BABYLON.Mesh.FRONTSIDE);
+  box.position.x = -1;
+  box.position.y = 1;
+  box.physicsImpostor = new BABYLON.PhysicsImpostor(box, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0.5, restitution: 0.9 }, scene);
+
+  const box2 = box.createInstance('box2');
+  box2.position.x = 1;
+  box2.position.y = 1;
+  box2.physicsImpostor = new BABYLON.PhysicsImpostor(box2, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0.5, restitution: 0.9 }, scene);
+
+  const box3 = box.createInstance('box3');
+  box3.position.x = 0;
+  box3.position.y = 3;
+  box3.physicsImpostor = new BABYLON.PhysicsImpostor(box3, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0.5, restitution: 0.9 }, scene);
 
   const ground = BABYLON.Mesh.CreateGround('ground1', 12, 12, 2, scene, false);
   ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
@@ -41,7 +52,7 @@ const createScene = () => {
 
   scene.onPointerDown = (e) => {
     if (e.button === MOUSE_LEFT) {
-      const spanner = sphere.createInstance("spanner");
+      const spanner = BABYLON.Mesh.CreateSphere('spanner', 16, 2, scene, false, BABYLON.Mesh.FRONTSIDE);
       spanner.position.x = camera.position.x;
       spanner.position.y = camera.position.y;
       spanner.position.z = camera.position.z;
