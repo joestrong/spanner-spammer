775c3c5792805f73bad7fd24f418108b16cf5b66
Joe Strong <joestrong@gmail.com>
2019-07-13 12:44:01 +0100

    Adds build watch task

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import * as BABYLON from 'babylonjs';
+
+const canvas = document.getElementById('renderCanvas');
+const engine = new BABYLON.Engine(canvas, true, {
+  preserveDrawingBuffer: true,
+  stencil: true
+});
+
+const createScene = function(){
+  const scene = new BABYLON.Scene(engine);
+
+  const camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 0, -10), scene);
+  camera.setTarget(BABYLON.Vector3.Zero());
+  camera.attachControl(canvas, false);
+
+  const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
+
+  const sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene, false, BABYLON.Mesh.FRONTSIDE);
+  sphere.position.y = 1;
+
+  const ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene, false);
+
+  return scene;
+};
+
+const scene = createScene();
+engine.runRenderLoop(function(){
+  scene.render();
+});
+window.addEventListener('resize', function(){
+  engine.resize();
+});
